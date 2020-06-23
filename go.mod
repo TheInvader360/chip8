5adb0b3a9754a5afd012a6c02b537d8984d7817c
Darren <theinvader360@gmail.com>
2020-06-23 17:54:14 +0100

    Add ebiten lib

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TheInvader360/chip8
 
 go 1.14
+
+require github.com/hajimehoshi/ebiten v1.11.3
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+	fullscreen   = false
+)
+
+type Game struct {
+}
+
+func NewGame() *Game {
+	g := &Game{}
+	return g
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0x00, 0x00, 0x00, 0xff})
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	ebiten.SetFullscreen(fullscreen)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("CHIP-8")
+	if err := ebiten.RunGame(NewGame()); err != nil {
+		panic(err)
+	}
 }
