16aa947ab84f841cfb780a9494acd4685559d953
Darren <theinvader360@gmail.com>
2020-06-24 00:23:51 +0100

    Load font, cycle stub, scan inputs, add references

--- main.go
+++ main.go
@@ -33,10 +33,29 @@ var (
 	soundTimer byte       //counts down to zero at 60hz
 	stack      [16]uint16 //store program counter in stack before jump/gosub
 	sp         uint16     //stack pointer to remember the level of stack used
-	key        [16]byte   //stores the current state of the hex keypad (0-F)
+	keys       [16]byte   //stores the current state of the hex keypad (0-F)
+	render     bool       //set by 0x00E0 (cls) and 0xDXYN (draw sprite)
 
-	colBlack = color.NRGBA{0x00, 0x00, 0x00, 0xff}
-	colGreen = color.NRGBA{0x00, 0xff, 0x00, 0xff}
+	col0    = color.NRGBA{0x00, 0x00, 0x00, 0xff}
+	col1    = color.NRGBA{0x00, 0xff, 0x00, 0xff}
+	fontset = []byte{
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+	}
 )
 
 type Game struct {
@@ -48,6 +67,14 @@ func NewGame() *Game {
 }
 
 func init() {
+	opcode = 0
+	memory = [4096]byte{}
+	for i := 0; i < len(fontset); i++ {
+		memory[i] = fontset[i]
+	}
+	v = [16]byte{}
+	i = 0
+	pc = 0x200
 	gfx = [gfxW * gfxH]byte{
 		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -82,6 +109,12 @@ func init() {
 		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	}
+	delayTimer = 0
+	soundTimer = 0
+	stack = [16]uint16{}
+	sp = 0
+	keys = [16]byte{}
+	render = true
 
 	if len(os.Args) < 2 {
 		log.Fatal("No ROM specified. Try: go run main.go rom/test/test_opcode.ch8")
@@ -96,12 +129,10 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(bytes)
 
 	for i := 0; i < len(bytes); i++ {
 		memory[i+0x200] = bytes[i]
 	}
-	fmt.Println(memory)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -112,19 +143,61 @@ func (g *Game) Update(screen *ebiten.Image) error {
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
 		os.Exit(0)
 	}
+
+	emulateCycle()
+
+	updateKeys()
+
 	return nil
 }
 
+func emulateCycle() {
+	//TODO:
+	// Fetch Opcode
+	// Decode Opcode
+	// Execute Opcode
+	// Update timers
+}
+
+func updateKeys() {
+	keys[0] = boolToByte(ebiten.IsKeyPressed(ebiten.Key1))
+	keys[1] = boolToByte(ebiten.IsKeyPressed(ebiten.Key2))
+	keys[2] = boolToByte(ebiten.IsKeyPressed(ebiten.Key3))
+	keys[3] = boolToByte(ebiten.IsKeyPressed(ebiten.Key4))
+	keys[4] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyQ))
+	keys[5] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyW))
+	keys[6] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyE))
+	keys[7] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyR))
+	keys[8] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyA))
+	keys[9] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyS))
+	keys[10] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyD))
+	keys[11] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyF))
+	keys[12] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyZ))
+	keys[13] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyX))
+	keys[14] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyC))
+	keys[15] = boolToByte(ebiten.IsKeyPressed(ebiten.KeyV))
+}
+
+func boolToByte(b bool) byte {
+	if b {
+		return 1
+	}
+	return 0
+}
+
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(colBlack)
-	for y := 0; y < gfxH; y++ {
-		for x := 0; x < gfxW; x++ {
-			if gfx[y*gfxW+x] == 1 {
-				ebitenutil.DrawRect(screen, float64(x)*pixelW, float64(y)*pixelH, pixelW, pixelH, colGreen)
+	if render {
+		screen.Fill(col0)
+		for y := 0; y < gfxH; y++ {
+			for x := 0; x < gfxW; x++ {
+				if gfx[y*gfxW+x] == 1 {
+					ebitenutil.DrawRect(screen, float64(x)*pixelW, float64(y)*pixelH, pixelW, pixelH, col1)
+				}
 			}
 		}
 	}
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
+	fmt.Println(keys)
 }
 
 func main() {
---
+++ reference/reference.md
@@ -0,0 +1,7 @@
+## Reference
+
+* https://en.wikipedia.org/wiki/CHIP-8
+* http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+* https://raw.githubusercontent.com/TheInvader360/chip8/reference/master/cowgod.htm
+* https://raw.githubusercontent.com/TheInvader360/chip8/reference/master/davidwinter.pdf
+
